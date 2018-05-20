f6dba369524884526351fdf57f53915ac432f931
w-v <wtv@protonmail.ch>
2018-05-20 20:45:14 +0200

    chunk grid *0.2

--- src/Terrain.cpp
+++ src/Terrain.cpp
@@ -13,12 +13,12 @@ Terrain::Terrain(){
 
 void Terrain::compute_indices(){
   for(unsigned int k=0;k<NB_CHUNK_PER_SIDE*NB_CHUNK_PER_SIDE;k++){
-    for(unsigned int i=0;i<CHUNK_SIZE-1;i++){
-      for(unsigned int j=0;j<CHUNK_SIZE;j++){
-        m_Indices.push_back((CHUNK_SIZE * CHUNK_SIZE * k) + j + (CHUNK_SIZE * i));
-        m_Indices.push_back((CHUNK_SIZE * CHUNK_SIZE * k) + j + (CHUNK_SIZE * (i + 1)));
+    for(unsigned int i=0;i<(CHUNK_SIZE * 5)-1;i++){
+      for(unsigned int j=0;j<(CHUNK_SIZE*5);j++){
+        m_Indices.push_back((CHUNK_SIZE*5 * CHUNK_SIZE*5 * k) + j + (CHUNK_SIZE*5 * i));
+        m_Indices.push_back((CHUNK_SIZE*5 * CHUNK_SIZE*5 * k) + j + (CHUNK_SIZE*5 * (i + 1)));
       }
-      m_Indices.push_back(CHUNK_SIZE*CHUNK_SIZE*NB_CHUNK_PER_SIDE*NB_CHUNK_PER_SIDE);
+      m_Indices.push_back(5*5*CHUNK_SIZE*CHUNK_SIZE*NB_CHUNK_PER_SIDE*NB_CHUNK_PER_SIDE);
     }
   }
 }
@@ -32,23 +32,23 @@ void Terrain::initload(glm::vec2 center){
         glm::vec3(0.0f, 0.0f, 0.0f))); 
     }
   }*/
-  int offset_x;
-  int offset_y = -1;
+  float offset_x;
+  float offset_y = -1;
   for(int k=0;k<NB_CHUNK_PER_SIDE;){
     offset_x = -1;
     for(int l=0;l<NB_CHUNK_PER_SIDE;){
-      for(int i=0;i<CHUNK_SIZE;i++){
-        for(int j=0;j<CHUNK_SIZE;j++){
-          printf("x:%d, z:%d \n", j+offset_x, i+offset_y);
+      for(float i=0.0f;i<CHUNK_SIZE;i+=0.2f){
+        for(float j=0.0f;j<CHUNK_SIZE;j+=0.2f){
+          printf("x:%f, z:%f \n", j+offset_x, i+offset_y);
           m_Positions.push_back(Vertex( glm::vec3(j + offset_x, 0, i + offset_y)));
         }
       }
 
       l++;
-      offset_x = (-1) + (l * (CHUNK_SIZE-1));
+      offset_x = (-1) + (l * (CHUNK_SIZE-0.2f));
     }
     k++;
-    offset_y = (-1) + (k * (CHUNK_SIZE-1));
+    offset_y = (-1) + (k * (CHUNK_SIZE-0.2f));
   }
 }
 
