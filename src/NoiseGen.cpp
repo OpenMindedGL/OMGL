2af39a984f866400fc75059f8bb0ab797a829532
trichico <p.tomas431@laposte.net>
2018-06-01 10:29:11 +0200

    cleaner script

--- src/NoiseGen.cpp
+++ src/NoiseGen.cpp
@@ -3,22 +3,22 @@
 
 NoiseGen::NoiseGen(){
   zoom = 3.0f;
-  biomes_size_coef = 50.0f;
-  nbOctave = 6;
+  biomes_size_coef = 80.0f;
+  nbOctave = 8;
   lacunarity = 2.00f;
   persistence = 0.5f;
   seed = 69;
-  m_NbBiomes = 5;
+  m_NbBiomes = 7;
 
-  //m_Biomes.SetNoiseType(FastNoise::Cellular);
-  //m_Biomes.SetFrequency(pow(lacunarity, 1) * 0.01f * (1.0f/biomes_size_coef));
+  FastNoise my_noise(54);
+  my_noise.SetFrequency(pow(lacunarity, 1) * 0.01f * (1.0f/10.0f));
+
   for(int l=0; l < m_NbBiomes - 1; l++){
-    for(unsigned int k=0;k < 8;k++){
-      FastNoise temp_noise(seed + k + l + (4*nbOctave));
-      temp_noise.SetNoiseType(FastNoise::Simplex);
-      temp_noise.SetFrequency(pow(lacunarity, k) * 0.01f * (1.0f/biomes_size_coef));
-      m_Biomes.push_back(temp_noise);
-    }
+    FastNoise temp_noise(seed + l + (3*nbOctave));
+    temp_noise.SetNoiseType(FastNoise::SimplexFractal);
+    temp_noise.SetFractalOctaves(8);
+    temp_noise.SetFrequency(lacunarity * 0.01f * (1.0f/biomes_size_coef));
+    m_Biomes.push_back(temp_noise);
   }
 
   for(unsigned int k=0;k < nbOctave;k++){
@@ -28,9 +28,9 @@ NoiseGen::NoiseGen(){
     temp_noise.SetNoiseType(FastNoise::Simplex);
     temp_noise2.SetNoiseType(FastNoise::Simplex);
     temp_noise3.SetNoiseType(FastNoise::Simplex);
-    temp_noise.SetFrequency(pow(lacunarity, k) * 0.01f * (1.0f/zoom) );
-    temp_noise2.SetFrequency(pow(lacunarity, k) * 0.01f * (1.0f/zoom) );
-    temp_noise3.SetFrequency(pow(lacunarity, k) * 0.01f * (1.0f/zoom) );
+    temp_noise.SetFrequency(pow(lacunarity, k) * 0.01f / zoom);
+    temp_noise2.SetFrequency(pow(lacunarity, k) * 0.01f / zoom);
+    temp_noise3.SetFrequency(pow(lacunarity, k) * 0.01f / zoom);
     simplex_fractal.push_back(temp_noise);
     simplex_fractal2.push_back(temp_noise2);
     simplex_fractal3.push_back(temp_noise3);
@@ -39,34 +39,31 @@ NoiseGen::NoiseGen(){
 
 float NoiseGen::compute(float x, float y){
   int biome_choice = 0;
-  float ret_value = 0.15f*10.0f;
+  float depth_river_coef = 10.0f;
+  float width_river = 0.15f;
+  float ret_value = width_river * depth_river_coef;
   float temp;
   float m = 0;
-  int d = 0;
-  for (int i = 0; i < m_NbBiomes - 1; i++){
-    for(std::vector<FastNoise>::iterator iter_noise = m_Biomes.begin() + i; 
-    (iter_noise < m_Biomes.end() + i + 5); iter_noise++){
-      m += (*iter_noise).GetNoise(x*10,y*10) * pow(persistence, d); 
-      d++;
-    }
+  for(std::vector<FastNoise>::iterator iter_noise = m_Biomes.begin(); 
+  iter_noise < m_Biomes.end(); iter_noise++){
+    m += (*iter_noise).GetNoise(x*10,y*10); 
     if (m > 0)
       biome_choice += 1;
-    if ((m > -0.15f) && (m <= 0.0f)){
-      temp = m*(-1)*10.0f;
+    if ((m > (-1) * width_river) && (m <= 0.0f)){
+      temp = m * (-1) * depth_river_coef;
       if (ret_value > temp)
         ret_value = temp;
     }
-    else if ((m < 0.15f) && (m >= 0.0f)){
-      temp = m*10.0f;
+    else if ((m < width_river) && (m >= 0.0f)){
+      temp = m * depth_river_coef;
       if (ret_value > temp)
         ret_value = temp;
     }
     m = 0;
-    d = 0;
   }
 
-  if (ret_value <= 0.12f){
-    ret_value = 0.12f;
+  if (ret_value <= width_river * 0.8f){
+    ret_value = width_river * 0.8f;
   }
   else{
     switch (biome_choice){
@@ -132,28 +129,18 @@ float NoiseGen::compute_biome3(float x, float y){
 
 float NoiseGen::compute_biome4(float x, float y){
   float a = 0;
-  float b = 0;
-  float c = 0;
   int d = 0;
   float temp_lacunarity = 4.915f;
   float temp_persistence = 0.187f;
-  std::vector<FastNoise>::iterator iter_noise2 = simplex_fractal2.begin();
-  std::vector<FastNoise>::iterator iter_noise3 = simplex_fractal3.begin();
   for(std::vector<FastNoise>::iterator iter_noise = 
   simplex_fractal.begin(); 
   (iter_noise < simplex_fractal.end()); iter_noise++){
-    (*iter_noise).SetFrequency(pow(temp_lacunarity, d) * 0.006f * (1.0f/zoom) );
-    (*(iter_noise2 + d)).SetFrequency(pow(temp_lacunarity, d) * 0.006f * (1.0f/zoom) );
-    (*(iter_noise3 + d)).SetFrequency(pow(temp_lacunarity, d) * 0.006f * (1.0f/zoom) );
+    (*iter_noise).SetFrequency(pow(temp_lacunarity, d) * 0.006f / zoom);
     a += glm::abs((*iter_noise).GetNoise(x*10,y*10) * pow(temp_persistence, d)); 
-    b += glm::abs((*(iter_noise2 + d)).GetNoise(x*10,y*10) * pow(temp_persistence, d)); 
-    c += glm::abs((*(iter_noise3 + d)).GetNoise(x*10,y*10) * pow(temp_persistence, d)); 
-    (*iter_noise).SetFrequency(pow(lacunarity, d) * 0.01f * (1.0f/zoom) );
-    (*(iter_noise2 + d)).SetFrequency(pow(lacunarity, d) * 0.01f * (1.0f/zoom) );
-    (*(iter_noise3 + d)).SetFrequency(pow(lacunarity, d) * 0.01f * (1.0f/zoom) );
+    (*iter_noise).SetFrequency(pow(lacunarity, d) * 0.01f / zoom);
     d++;
   }
-  return a*b*c*3.0f*zoom;
+  return a*a*a*1.5f*zoom;
 }
 
 float NoiseGen::compute_biome5(float x, float y){
@@ -161,7 +148,7 @@ float NoiseGen::compute_biome5(float x, float y){
 }
 
 float NoiseGen::compute_biome6(float x, float y){
-  FastNoise my_noise(seed + nbOctave*3);
+  FastNoise my_noise(seed + nbOctave*4);
   my_noise.SetNoiseType(FastNoise::Simplex);
   my_noise.SetFrequency(0.8f/zoom);
   float a = 0;
@@ -171,18 +158,18 @@ float NoiseGen::compute_biome6(float x, float y){
   simplex_fractal.begin(); 
   (iter_noise < simplex_fractal.end()); iter_noise++){
     temp_lacunarity = my_noise.GetNoise(x,y) + 0.5f;
-    (*iter_noise).SetFrequency(pow(temp_lacunarity, d) * 0.01f * (1.0f/(zoom/5.0f))/5.0f);
+    (*iter_noise).SetFrequency(pow(temp_lacunarity, d) * 0.01f / zoom);
     a += (*iter_noise).GetNoise(x*10,y*10) * pow(persistence, d); 
-    (*iter_noise).SetFrequency(pow(lacunarity, d) * 0.01f * (1.0f/zoom) );
+    (*iter_noise).SetFrequency(pow(lacunarity, d) * 0.01f / zoom);
     d++;
   }
   return a*a*a*zoom/5.0f;
 }
 
 float NoiseGen::compute_biome7(float x, float y){
-  FastNoise my_noise(seed + nbOctave*3);
+  FastNoise my_noise(seed + nbOctave*4 + 1);
   my_noise.SetNoiseType(FastNoise::Simplex);
-  FastNoise my_noise2(seed + nbOctave*3);
+  FastNoise my_noise2(seed + nbOctave*4 + 2);
   my_noise2.SetNoiseType(FastNoise::Simplex);
   my_noise.SetFrequency(my_noise2.GetNoise(x,y)/zoom);
   float a = 0;
@@ -192,9 +179,9 @@ float NoiseGen::compute_biome7(float x, float y){
   simplex_fractal.begin(); 
   (iter_noise < simplex_fractal.end()); iter_noise++){
     temp_lacunarity = my_noise.GetNoise(x,y) + 0.5f;
-    (*iter_noise).SetFrequency(pow(temp_lacunarity, d) * 0.01f * (1.0f/(zoom/5.0f))/5.0f);
+    (*iter_noise).SetFrequency(pow(temp_lacunarity, d) * 0.01f / zoom);
     a += (*iter_noise).GetNoise(x*10,y*10) * pow(persistence, d); 
-    (*iter_noise).SetFrequency(pow(lacunarity, d) * 0.01f * (1.0f/zoom) );
+    (*iter_noise).SetFrequency(pow(lacunarity, d) * 0.01f / zoom);
     d++;
   }
   return a*a*a*zoom/5.0f;
